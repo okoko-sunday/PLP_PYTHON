5f6c927b488a930a66329869181847d6d21d0309
okoko-sunday <okoko.sunday17@gmail.com>
2024-11-05 03:52:55 +0100

    Python Week-1 Assignment

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input('Enter your name: ')
+age = int(input('Enter your age: '))
+location = input('Enter your location: ') 
+print(f'\nHello {name}, you\'re {age} years old and here is your address; {location}.')
