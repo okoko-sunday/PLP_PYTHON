e670ab7e8afeb258d998337c36e9d833b38de43d
okoko-sunday <okoko.sunday17@gmail.com>
2024-11-11 03:28:30 +0100

    week1 and week2 assignment re_done

---
+++ WEEK_2_ASSIGNMENT/plp.py
@@ -0,0 +1,20 @@
+'''WEEK 2 ASSIGNMENT'''
+
+my_list = []
+
+data = [10,20,30,40]
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+my_list.insert(1,15)
+
+my_list.extend([50,60,70])
+
+my_list.pop()
+
+my_list.sort()
+
+print(my_list.index(30))
+print(my_list[3])
